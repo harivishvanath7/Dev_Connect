cedda08da1accf280d8ae717951428ae11f3b1b0
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-05 12:42:00 +0530

    Updated Route imports for profile in  Server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
+const profileRoutes = require("./routes/profileRoutes");
 
 require("dotenv").config();
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/profile", profileRoutes);
 
 app.get("/", (req, res) => {
     res.send("DevConnectttt Babyyy...");
