81afbdd5a89b9a71f13a8cbb1aa23cf07930105b
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-05 10:22:59 +0530

    chore: Updated dependencies and server imports.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 require("dotenv").config();
 
 const app = express();
@@ -10,6 +12,8 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("DevConnectttt Babyyy...");
