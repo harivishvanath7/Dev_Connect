6e0f6a581ff514b5cfe035a5848b7e1d17001092
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-06 22:00:16 +0530

    feat(server): Added Websocket connection with socket.io

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const http = require("http");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const { Server } = require("socket.io");
 
+// API Routes
 const authRoutes = require("./routes/authRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const postRoutes = require("./routes/postRoutes");
@@ -13,6 +16,30 @@ require("dotenv").config();
 const app = express();
 connectDB();
 
+// Create HTTP server
+const server = http.createServer(app);
+
+// Attach Socket.io 
+const io = new Server(server, { 
+    cors: { 
+        origin: "*"
+    } 
+});
+
+// Socket connection
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    // Listen for message event
+    socket.on("sendMessage", (data) => {
+        io.emit("receiveMessage", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+
+});
 
 app.use(cors());
 app.use(express.json());
@@ -29,6 +56,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, (req, res) => {
-    console.log(`App running at PORT:${PORT}...`);
+server.listen(PORT, () => {
+    console.log(`Server running at PORT:${PORT}...`);
 });
