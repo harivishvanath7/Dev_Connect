48daa20ff7661ce5b414a76f96eb546dcfe75e4f
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-23 17:08:58 +0530

    feat(layout): Added Layout for the App page with navbar.

---
+++ client/src/layout/MainLayout.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-(--color-background)">
+     {/* NAVBAR */}
+      <nav className="bg-white shadow p-4 flex justify-between">
+        <h1 className="text-(--color-primary) font-bold">DevConnect</h1>
+        
+        <div className="flex gap-4">
+          <Link to="/">Feed</Link>
+          <Link to="/profile">Profile</Link>
+        </div>
+      </nav>
+     {/* CONTENT */}
+      <div className="p-4 max-w-3xl mx-auto">{children}</div>
+    </div>
+  );
+};
+
+export default MainLayout;
