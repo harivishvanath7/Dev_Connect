4cb1f4dab510c6190463cc9a14c023e834afbcd0
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-23 17:10:33 +0530

    chore: Updated Layout Routes For Feed and Profile pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
 import Feed from "./pages/feed/Feed";
+import Profile from "./pages/profile/Profile";
+import MainLayout from "./layout/MainLayout";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route
+          path="/"
+          element={
+            <MainLayout>
+              <Feed />
+            </MainLayout>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
