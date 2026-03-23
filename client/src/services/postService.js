8dc89f4ba8ca9f8b8586e02005681265f6e33dd9
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-23 15:27:00 +0530

    feat(postService-client): Added API service for posts in client.

---
+++ client/src/services/postService.js
@@ -0,0 +1,23 @@
+import API from "../api/axios";
+
+const getPosts = async () => {
+  const res = await API.get("/posts");
+  return res.data;
+};
+
+const createPost = async (data) => {
+  const res = await API.post("/posts", data);
+  return res.data;
+};
+
+const likePost = async (id) => {
+  const res = await API.post(`/posts/like/${id}`);
+  return res.data;
+};
+
+const addComment = async (id, text) => {
+  const res = await API.post(`/posts/comment/${id}`, { text });
+  return res.data;
+};
+
+export { createPost, getPosts, likePost, addComment };
