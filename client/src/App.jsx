3b257125c66747e13e8ff426538c22737a338385
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-23 12:47:48 +0530

    chore: Updated routes for Login, Aignup.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<h1>Feed</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
