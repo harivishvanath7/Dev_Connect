14174e4f9234109fc00cd87b3d83dbe074648059
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-23 15:29:45 +0530

    chore: Updated routes.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
+import Feed from "./pages/feed/Feed";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Feed</h1>} />
+        <Route path="/" element={<Feed />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
