3e749ed9b8bbb411acc3d3f72e2309f0fbcd0faa
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-23 17:23:21 +0530

    feat(edit-profile): Added Edit Profile Option in Profile Page.

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
-import { createProfile, getProfileByUser } from "../../services/profileService";
+import {
+  createProfile,
+  getProfileByUser,
+  updateProfile,
+} from "../../services/profileService";
 
 const Profile = () => {
   const [form, setForm] = useState({
@@ -11,11 +15,16 @@ const Profile = () => {
     github: "",
   });
 
+  // State for editing
+  const [isEditing, setIsEditing] = useState(false);
+
   const [profile, setProfile] = useState(null);
 
   const user = JSON.parse(localStorage.getItem("user"));
 
   const fetchProfile = async () => {
+    if (!user?.user?.id) return;
+    
     try {
       const data = await getProfileByUser(user?.user?.id);
       setProfile(data);
@@ -36,28 +45,47 @@ const Profile = () => {
     e.preventDefault();
 
     try {
-      const data = await createProfile(form);
+      let data;
+
+      if (isEditing) {
+        data = await updateProfile(form);
+      } else {
+        data = await createProfile(form);
+      }
+
       setProfile(data);
+      setIsEditing(false);
     } catch (error) {
       console.log(error);
     }
   };
 
-  if (profile) {
+  if (profile && !isEditing) {
     return (
       <div className="bg-white p-6 rounded shadow">
         <h2 className="text-xl text-(--color-primary)">{profile.user?.name}</h2>
 
         <p>{profile.bio}</p>
-        <p>
-          <strong>Skills:</strong> {profile.skills}
-        </p>
-        <p>
-          <strong>Company:</strong> {profile.company}
-        </p>
-        <p>
-          <strong>Location:</strong> {profile.location}
-        </p>
+        <p><strong>Skills:</strong> {profile.skills}</p>
+        <p><strong>Company:</strong> {profile.company}</p>
+        <p><strong>Location:</strong> {profile.location}</p>
+
+        <button
+          onClick={() => {
+            setIsEditing(true);
+            setForm({
+              bio: profile.bio || "",
+              skills: profile.skills || "",
+              company: profile.company || "",
+              location: profile.location || "",
+              website: profile.website || "",
+              github: profile.github || "",
+            }); // preload form
+          }}
+          className="mt-4 bg-(--color-secondary) px-4 py-2 rounded"
+        >
+          Edit Profile
+        </button>
       </div>
     );
   }
@@ -69,24 +97,28 @@ const Profile = () => {
       <input
         name="bio"
         placeholder="Bio"
+        value={form.bio}
         onChange={handleChange}
         className="block w-full mb-2 p-2 border"
       />
       <input
         name="skills"
         placeholder="Skills (comma seperated)"
+        value={form.skills}
         onChange={handleChange}
         className="block w-full mb-2 p-2 border"
       />
       <input
         name="company"
         placeholder="Company"
+        value={form.company}
         onChange={handleChange}
         className="block w-full mb-2 p-2 border"
       />
       <input
         name="location"
         placeholder="Location"
+        value={form.location}
         onChange={handleChange}
         className="block w-full mb-2 p-2 border"
       />
